f80fb95f8c374ed291df8deee3555b1c35a75145
pop <chaip2538@gmail.com>
2015-12-06 12:36:35 +0700

    ListView สำเร็จ

--- app/src/main/java/apppj/chaiyapornp/mytraffic/MainActivity.java
+++ app/src/main/java/apppj/chaiyapornp/mytraffic/MainActivity.java
@@ -25,7 +25,57 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //ListView Controller
+        listViewController();
+
     }   // Main Method นี่คือ เมธอด
+
+    private void listViewController() {
+
+
+        //For Icon
+        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04,
+                R.drawable.traffic_05, R.drawable.traffic_06,
+                R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10,
+                R.drawable.traffic_11, R.drawable.traffic_12,
+                R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16,
+                R.drawable.traffic_17, R.drawable.traffic_18,
+                R.drawable.traffic_19, R.drawable.traffic_20};
+        //for title
+        String[] titleSrting = new String[20];
+        titleSrting[0] = "หัวข้อที่ 1";
+        titleSrting[1] = "หัวข้อที่ 2";
+        titleSrting[2] = "หัวข้อที่ 3";
+        titleSrting[3] = "หัวข้อที่ 4";
+        titleSrting[4] = "หัวข้อที่ 5";
+        titleSrting[5] = "หัวข้อที่ 6";
+        titleSrting[6] = "หัวข้อที่ 7";
+        titleSrting[7] = "หัวข้อที่ 8";
+        titleSrting[8] = "หัวข้อที่ 9";
+        titleSrting[9] = "หัวข้อที่ 10";
+        titleSrting[10] = "หัวข้อที่ 11";
+        titleSrting[11] = "หัวข้อที่ 12";
+        titleSrting[12] = "หัวข้อที่ 13";
+        titleSrting[13] = "หัวข้อที่ 14";
+        titleSrting[14] = "หัวข้อที่ 15";
+        titleSrting[15] = "หัวข้อที่ 16";
+        titleSrting[16] = "หัวข้อที่ 17";
+        titleSrting[17] = "หัวข้อที่ 18";
+        titleSrting[18] = "หัวข้อที่ 19";
+        titleSrting[19] = "หัวข้อที่ 20";
+
+
+        //For Detail
+
+        String[] detailString = getResources().getStringArray(R.array.detail_short);
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, titleSrting, detailString, intIcon);
+
+        trafficListView.setAdapter(objMyAdapter);
+
+    }   //ListView Controller
 
     private void buttonController() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
@@ -34,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
                 //Sound Effect
 
-                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.sheep);
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.sheep);
                 buttonMediaPlayer.start();
                 //Intent to WebView
                 Intent objIntent = new Intent(Intent.ACTION_VIEW);
