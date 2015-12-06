5201b77c8a057f0e8eef321aadd192a5963210a5
pop <chaip2538@gmail.com>
2015-12-06 14:51:16 +0700

    ใส่เสียงสำเร็จ

--- app/src/main/java/apppj/chaiyapornp/mytraffic/SplashActivity.java
+++ app/src/main/java/apppj/chaiyapornp/mytraffic/SplashActivity.java
@@ -1,13 +1,31 @@
 package apppj.chaiyapornp.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
-}
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent objIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(objIntent);
+                finish();
+            }
+        }, 8000);
+
+        MediaPlayer introPlayer = MediaPlayer.create(getBaseContext(),R.raw.intro_start_horse);
+        introPlayer.start();
+
+    }   //Main Mathod
+}   //Main Class
